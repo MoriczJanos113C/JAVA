9729618304319fa417b4796086541317cc6006bf
MoriczJanos113C <71004440+MoriczJanos113C@users.noreply.github.com>
2021-03-17 12:22:18 +0100

    feltöltés

---
+++ fajlkezeles/Fajlkezelesalapok.java
@@ -0,0 +1,39 @@
+package fajlkezelesalapok;
+
+import java.io.*;
+
+public class Fajlkezelesalapok {
+
+    public static void main(String[] args) {
+        
+    RandomAccessFile raf;
+    String sor;
+    String[] nevek = new String[19];
+ 
+    try
+    {
+      raf = new RandomAccessFile("C:\\Users\\moric\\Desktop\\nevek.txt","r");
+      
+      int db = 0;
+      sor = raf.readLine();
+ 
+      while( sor != null )
+      {
+        nevek[db] = sor;
+        db++;
+        sor = raf.readLine();
+      }
+      
+      raf.close();
+    }
+    catch( IOException e )
+    {
+      System.err.println("HIBA");
+    }
+    for( String str : nevek )
+    {
+      System.out.println(str);
+    }
+  }
+}
+        
